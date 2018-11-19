328519b3a47119336a231ba81ee033f15937facb
Pablo <pablo_rodriguez833@hotmail.com>
2018-11-18 21:14:37 -0300

    Add conductor and producto

---
+++ src/DAOS/impl/ConductorDAOImpl.java
@@ -0,0 +1,35 @@
+package DAOS.impl;
+
+import DAOS.ConductorDAO;
+import ENTIDADES.Conductor;
+import EXCEPTIONS.RadioException;
+
+import java.util.List;
+
+public class ConductorDAOImpl implements ConductorDAO{
+
+    @Override
+    public void insert(Conductor conductor) throws RadioException {
+
+    }
+
+    @Override
+    public void update(Conductor conductor) throws RadioException {
+
+    }
+
+    @Override
+    public Conductor get(int codigo) throws RadioException {
+        return null;
+    }
+
+    @Override
+    public void delete(int codigo) throws RadioException {
+
+    }
+
+    @Override
+    public List<Conductor> getAll() throws RadioException {
+        return null;
+    }
+}
---
+++ src/DAOS/impl/ProductorDAOImpl.java
@@ -0,0 +1,35 @@
+package DAOS.impl;
+
+import DAOS.ProductorDAO;
+import ENTIDADES.Productor;
+import EXCEPTIONS.RadioException;
+
+import java.util.List;
+
+public class ProductorDAOImpl implements ProductorDAO {
+
+    @Override
+    public void insert(Productor productor) throws RadioException {
+
+    }
+
+    @Override
+    public void update(Productor productor) throws RadioException {
+
+    }
+
+    @Override
+    public Productor get(int codigo) throws RadioException {
+        return null;
+    }
+
+    @Override
+    public void delete(int codigo) throws RadioException {
+
+    }
+
+    @Override
+    public List<Productor> getAll() throws RadioException {
+        return null;
+    }
+}
